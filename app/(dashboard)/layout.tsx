4beb450cebe6f684558c9a100edfbdc40ebb6873
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2023-12-19 23:46:26 +0530

    Layout and Page files updated

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,11 @@
+import NewTodoForm from '@/components/NewTodoForm'
 import React from 'react'
 
 const DashoboardLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
             <h1>DashoboardLayout</h1>
+            <div><NewTodoForm/></div>
             <div>{children}</div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-'use client'
-
-import NewTodoForm from "@/components/NewTodoForm";
-
-const getData = async () => {
-  await new Promise<void>((resolve) => setTimeout(() => (resolve(), 2000)));
-  return { data: [1, 2, 3] };
-}
+import React from 'react'
 
 const Home = () => {
-  const data = getData();
-  console.log(data);
-
-  return (<div>
-    <NewTodoForm />
-  </div>)
+  return (
+    <div>Home</div>
+  )
 }
 
-
-export default Home;
+export default Home
