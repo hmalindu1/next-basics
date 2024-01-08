a3db1113d377e5a340beebe98a482d602c96f58a
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-01-08 11:12:31 +0530

    Todo update function

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,9 +1,19 @@
 'use client'
-
-import React from 'react'
+import React, { useTransition } from 'react'
+import { completeTodo } from '@/utils/actions'
 
 const Todo = ({ todo }) => {
-  return <div>{todo.content}</div>
+  const [isPending, startTransition] = useTransition()
+  return (
+    <div
+      className={`border border-black/20 cursor-pointer ${
+        todo.completed ? 'line-through text-gray-600' : ''
+      }`}
+      onClick={() => startTransition(() => completeTodo(todo.id))}
+    >
+      {todo.content}
+    </div>
+  )
 }
 
 export default Todo
