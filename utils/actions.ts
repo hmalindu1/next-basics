8bf795c30c47b890266f347dc32d4a39a72c9886
Hashan Hemachandra <hemachandra.hashan@gmail.com>
2024-01-08 11:12:06 +0530

    added update function

--- utils/actions.ts
+++ utils/actions.ts
@@ -2,6 +2,19 @@
 import db from '@/utils/db'
 import { revalidatePath } from 'next/cache'
 
+export const completeTodo = async (id) => {
+  await db.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      completed: true,
+    },
+  })
+
+  revalidatePath('/todos')
+}
+
 export const newTodo = async (formData) => {
   const todo = await db.todo.create({
     data: {
